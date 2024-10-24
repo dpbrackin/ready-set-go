1b7dc6d36b6e883bdf877d0ddc83b9665d217773
dpbrackin <dp.brackin@gmail.com>
2024-10-24 12:23:48 +0800

    feat: auth handlers

--- main.go
+++ main.go
@@ -1,17 +1,164 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/dpbrackin/ready-set-go/auth"
+	"github.com/dpbrackin/ready-set-go/db/generated"
+	"github.com/dpbrackin/ready-set-go/db/repositories"
 	"github.com/dpbrackin/ready-set-go/router"
+	"github.com/jackc/pgx/v5"
 )
 
+type RealClock struct{}
+
+func (r *RealClock) Now() time.Time {
+	return time.Now()
+}
+
 func main() {
+	ctx := context.Background()
+	conn, err := pgx.Connect(ctx, os.Getenv("DB_DSN"))
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	q := generated.New(conn)
+
+	authService := auth.NewAuthService(auth.NewAuthServiceParams{
+		Repository: repositories.NewPGAuthRepository(q),
+		Clock:      &RealClock{},
+	})
+
+	authHandlers := &AuthHandlers{
+		Srv: authService,
+	}
+
 	root := router.NewRootRouter()
 	root.Use(LoggingMiddleware)
 
 	unauthenticatedGroup := root.Group("")
-	unauthenticatedGroup.RouteFunc("POST /login", func(w http.ResponseWriter, r *http.Request) {})
+	unauthenticatedGroup.RouteFunc("POST /login", authHandlers.Login)
+	unauthenticatedGroup.RouteFunc("POST /register", authHandlers.Register)
 
-	http.ListenAndServe(":3000", root.Mux())
+	authenticatedGroup := root.Group("")
+	authenticatedGroup.Use(AuthMiddleware(authService))
+	authenticatedGroup.RouteFunc("GET /whoami", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(r.Context().Value("user"))
+	})
+
+	addr := ":3000"
+
+	log.Printf("Listening on %s", addr)
+
+	err = http.ListenAndServe(addr, root.Mux())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+type AuthHandlers struct {
+	Srv *auth.AuthService
+}
+
+type LoginRequestBody struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type RegisterRequestBody struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func (handler *AuthHandlers) Login(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var body LoginRequestBody
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	user, err := handler.Srv.AuthenticateWithPassword(ctx, auth.PasswordCredentials{
+		Username: body.Username,
+		Password: body.Password,
+	})
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	session, err := handler.Srv.CreateSession(ctx, user)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	cookie := &http.Cookie{
+		Name:     "sessionID",
+		Value:    session.ID,
+		Quoted:   false,
+		Expires:  session.ExpiresAt,
+		MaxAge:   int(session.ExpiresAt.Sub(time.Now()).Seconds()),
+		Secure:   true,
+		HttpOnly: true,
+		SameSite: http.SameSiteLaxMode,
+	}
+
+	http.SetCookie(w, cookie)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(session)
+}
+
+func (handler *AuthHandlers) Register(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	var body RegisterRequestBody
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	_, err = handler.Srv.Register(ctx, auth.PasswordCredentials{
+		Username: body.Username,
+		Password: body.Password,
+	})
+
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
--- middlewares.go
+++ middlewares.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/dpbrackin/ready-set-go/auth"
+	"github.com/dpbrackin/ready-set-go/router"
 )
 
 type ResponseWritter struct {
@@ -30,4 +34,32 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 		log.Printf("[%d] %v %v", responseWriter.statusCode, path, time.Since(start))
 
 	})
+}
+
+// AuthMidleware checks for a loged in user and passes it into the context.
+// If their is no logged in user, it will reject the request with a 401.
+func AuthMiddleware(srv *auth.AuthService) router.Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			sessionID, err := r.Cookie("sessionID")
+
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			user, err := srv.AuthenticateSession(r.Context(), sessionID.Value)
+
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte(err.Error()))
+				return
+			}
+
+			ctx := context.WithValue(r.Context(), "user", user)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
 }
